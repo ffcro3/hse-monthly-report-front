c023ab081e1c15e0e7420110c24a31c0bc9fc246
Fabricio Rocha <fabriciofrocha87@gmail.com>
2019-12-04 01:58:42 -0300

    Added dotenv

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
+import 'dotenv/config';
 import auth from '../config/authAPI';
 
 const api = axios.create({
-  baseURL: 'http://191.252.111.174:3030',
+  baseURL: process.env.REACT_APP_API_URL,
   headers: {
     Authorization: `bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVkZTJkMGM5OGY0ZjUxZjE2ZjUyYzc5YiIsImlhdCI6MTU3NTIyNTExNywiZXhwIjoxNTc1ODI5OTE3fQ.aFuj5u5MKnHiMNEuxcJqWNXFVjWMxbm4hAlMblteeaI`,
   },
