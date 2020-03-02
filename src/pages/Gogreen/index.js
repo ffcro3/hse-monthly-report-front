ef425d83c515da6d2d5aa5437f37fde7e239159f
Fabricio Rocha <fabriciofrocha87@gmail.com>
2020-03-02 16:31:42 -0400

    Date adjusments for 04/03/2020

--- src/pages/Gogreen/index.js
+++ src/pages/Gogreen/index.js
@@ -171,7 +171,7 @@ export default class Report extends Component {
                   <PageTitle>
                     Report Mensal de HSE - {reportData.siteName}
                   </PageTitle>
-                  <PageSubTitle>Data Final: 05/03/2020</PageSubTitle>
+                  <PageSubTitle>Data Final: 04/03/2020</PageSubTitle>
                   <PageSubTitle>
                     Observações: Cada página representa um item do report.
                     Preencha as informações e as mesmas serão salvas a cada
