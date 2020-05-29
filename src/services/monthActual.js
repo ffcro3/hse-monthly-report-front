6a13b090920e2a134cbc3e771306b3b0198def55
Fabricio Rocha <fabriciofrocha87@gmail.com>
2020-05-29 10:07:11 -0300

    Month Changes for deploy

--- src/services/monthActual.js
+++ src/services/monthActual.js
@@ -1,9 +1,9 @@
 export const monthData = {
-  actualMonth: 'Abril',
-  actualMonthNumber: '05',
+  actualMonth: 'Junho',
+  actualMonthNumber: '06',
   actualYearNumber: '2020',
-  finalDate: '05/05/2020',
-  monthAll: 'Maio 2020',
+  finalDate: '02/06/2020',
+  monthAll: 'Junho 2020',
   authorization:
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVlYWEwOWFlYTFmOWY5MGRjYmI5N2RmYiIsImlhdCI6MTU5MDA2OTg1MSwiZXhwIjoxNTkwNjc0NjUxfQ.INzVI852a-xcLPuim1WV7kTIPL3NaF3gCWrhF5_t1s4',
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVlYWEwOWFlYTFmOWY5MGRjYmI5N2RmYiIsImlhdCI6MTU5MDc1Njg3NiwiZXhwIjoxNTkxMzYxNjc2fQ.Wl4jYr9t6naO5erx_ooedS6pVqpzBc0HerStKE4CA34',
 };
